08ca7694571f74537ea608e27707ca6e1990419d
Dennis Thomasen <dennisthomasen@gmail.com>
2013-03-24 10:55:05 +0100

    Finding mp4 and mkv files

--- PMovies.py
+++ PMovies.py
@@ -1,1 +1,20 @@
-__author__ = 'Dennis'
+import configparser
+import os
+
+scriptDir = os.path.dirname(os.path.realpath(__file__))
+config = configparser.ConfigParser()
+print ("\nProperties: "+ config.read(scriptDir+'\\PMovies.properties')[0])
+fromDir = config.get("Paths","OriginalDir")
+toDir = config.get("Paths","MoveTo")
+mp4Files = []
+mkvFiles = []
+
+for r,d,f in os.walk(fromDir):
+    for files in f:
+        if files.endswith(".mp4"):
+            mp4Files.append(os.path.join(r,files))
+        if files.endswith(".mkv"):
+            mkvFiles.append(os.path.join(r,files))
+
+print("Found "+str(len(mp4Files))+" mp4 files.")
+print("Found "+str(len(mkvFiles))+" mkv files.")
