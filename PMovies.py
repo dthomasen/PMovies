971f7db6e6bb4ce2a768851717c783da985438bd
Dennis Thomasen <dennisthomasen@gmail.com>
2013-03-24 11:12:44 +0100

    Finding all relevant files

--- PMovies.py
+++ PMovies.py
@@ -6,15 +6,33 @@ config = configparser.ConfigParser()
 print ("\nProperties: "+ config.read(scriptDir+'\\PMovies.properties')[0])
 fromDir = config.get("Paths","OriginalDir")
 toDir = config.get("Paths","MoveTo")
+moveSamples = config.get("Config","MoveSamples")
 mp4Files = []
 mkvFiles = []
+aviFiles = []
+wmvFiles = []
+sampleFiles = []
 
 for r,d,f in os.walk(fromDir):
     for files in f:
-        if files.endswith(".mp4"):
-            mp4Files.append(os.path.join(r,files))
+        if "sample" in files.lower():
+           sampleFiles.append(os.path.join(r,files))
+        elif files.endswith(".mp4"):
+           mp4Files.append(os.path.join(r,files))
         if files.endswith(".mkv"):
-            mkvFiles.append(os.path.join(r,files))
+           mkvFiles.append(os.path.join(r,files))
+        if files.endswith(".avi"):
+           aviFiles.append(os.path.join(r,files))
+        if files.endswith(".wmv"):
+           wmvFiles.append(os.path.join(r,files))
 
-print("Found "+str(len(mp4Files))+" mp4 files.")
-print("Found "+str(len(mkvFiles))+" mkv files.")
+print("Found "+str(len(mp4Files))+" mp4 file(s).")
+print("Found "+str(len(mkvFiles))+" mkv file(s).")
+print("Found "+str(len(aviFiles))+" avi file(s).")
+print("Found "+str(len(wmvFiles))+" wmv file(s).")
+
+if moveSamples is "Yes":
+    print("Found "+str(len(sampleFiles))+" sample file(s)")
+else:
+    print("Skipping "+str(len(sampleFiles))+" sample file(s)")
+
